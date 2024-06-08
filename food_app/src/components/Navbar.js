f655cb7d1440827ca72a014fe7f7b784489c9d3d
Chetna <cmaske580@gmail.com>
2024-06-08 08:03:10 +0530

    navbar css done

--- food_app/src/components/Navbar.js
+++ food_app/src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import {useState} from 'react'
 import logo from '../assets/pizzaLogo.png'
 import { Link } from "react-router-dom";
-import Home from '../pages/Home';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import "../styles/Navbar.css"
 
 
 function Navbar() {
+    const [openLinks, setopenLinks]= useState(false);
+
+    const toggleNavbar = () =>{
+        setopenLinks(!openLinks);
+    }
+    
   return (
     <div className='navbar'>
-        <div className='leftSide'>
+        <div className='leftSide'id={openLinks ? "open" : "close"}>
             <img src={logo} />
+            <div className='hiddenLinks'>
+            <Link to="/">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contactUs">ContactUs</Link>
+            </div>
         </div>
         <div className='rightSide'>
             <Link to="/">Home</Link>
             <Link to="/menu">Menu</Link>
             <Link to="/about">About</Link>
             <Link to="/contactUs">ContactUs</Link>
+            <button onClick={toggleNavbar}> 
+                <ReorderIcon />
+            </button>
+            
         </div>  
     </div>
   )
