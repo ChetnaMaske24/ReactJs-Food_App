f95f6e5ef6961533e64203fba3367906c128e8de
Chetna <cmaske580@gmail.com>
2024-06-08 12:16:17 +0530

    Home Page Completed

--- food_app/src/pages/Home.js
+++ food_app/src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import HomeImage from '../assets/pizza.jpg'
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
+      <div className='headerContainer'
+      style={ {backgroundImage: `url(${HomeImage})`}}
+      >
+        <h1>Pro Pizzeria</h1>
+        <p>PIZZA TO FIT ANY TASTE</p>
+        <Link to = "/menu">
+        <button>ORDER NOW</button>
+        </Link>
+        
+      </div>
       
     </div>
   )
