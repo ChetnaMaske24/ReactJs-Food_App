2a408fd9f7e681821426b00f47211ebdd7357a85
Chetna <cmaske580@gmail.com>
2024-06-09 23:19:46 +0530

    coded upto menu

---
+++ food_app/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/BuffaloPizza.jpg";
+import Margherita from "../assets/paneerPizza.webp";
+import PedroTechSpecial from "../assets/TandooriPizza.jpg";
+import Vegan from "../assets/Pepperoni.png";
+import Pineapple from "../assets/veggie-pizza.jpg";
+import Expensive from "../assets/WorksPizza.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 15.99,
+  },
+  {
+    name: "Buffalo Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "Paneer Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Tandoori Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Veggie Pizza",
+    image: Pineapple,
+    price: 4.99,
+  },
+  {
+    name: "Works Pizza",
+    image: Expensive,
+    price: 1997.99,
+  },
+];
